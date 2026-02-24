b77b9d44cd89ac98262618a76275bc43a33a7a5e
Abhiram-Makkapati <makkapatiabhiram@gmail.com>
2026-02-24 17:31:37 -0600

    Add header file for residential processing functions

---
+++ residential.h
@@ -0,0 +1,10 @@
+#ifndef RESIDENTIAL_H
+#define RESIDENTIAL_H
+
+#include "init.h"
+#include "mahishmathicity.h"
+#include <vector>
+
+MahishmathiCity residentialProcess(MahishmathiCity& city, std::vector<std::vector<MahishmathiCity>>& Region, int x, int y);
+
+#endif
